1a3eb0ad3f8fa861fd9b2772f42442fc58424623
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-08 14:38:54 +0200

    fix: Optimized exercise

--- src/App.java
+++ src/App.java
@@ -2,28 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.print("Introduce un día de la semana: ");
         String dia = System.console().readLine();
-
-        switch (dia.toLowerCase()){
-            case  "lunes":
-                System.out.print("La primera asignatura de este dia es Sistemas Informáticos.");
-                break;
-            case "martes":
-                System.out.print("La primera asignatura de este dia es Programación.");
-                break;
-            case "miercoles":
-                System.out.print("La primera asignatura de este dia es Sistenmas Inforáticos.");
-                break;
-            case "miércoles":
-                System.out.print("La primera asignatura de este dia es Sistenmas Inforáticos.");
-                break;
-            case "jueves":
-                System.out.print("La primera asignatura de este dia es IPEI.");
-                break;
-            case "viernes":
-                System.out.print("La primera asignatura de este dia es Bases de Datos.");
-                break;
-            default:
-                System.out.print("No has insertado un dia de la semana valido.");
-        }
+        System.out.printf("""
+                El %s tienes clase de %s
+                """, dia.toLowerCase(),
+            switch(dia.toLowerCase()){
+                case "lunes"->"Sistemas Informáticos";
+                case "martes"->"Programación";
+                case "miercoles"->"Sistenmas Inforáticos";
+                case "miércoles"->"Sistenmas Inforáticos";
+                case "jueves"->"IPEI";
+                case "viernes"->"Bases de Datos";
+                default->"No hay clases este dia";
+            }
+        );
     }
 }
